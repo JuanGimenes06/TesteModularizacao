9d715385db00e1548188ae244d0f2e15952476cc
Juan <joaopbg06@gmail.com>
2023-10-27 08:10:58 -0300

    Initial commit

---
+++ cad.js
@@ -0,0 +1,7 @@
+let estadofuncao = require('./estado');
+let idadefuncao = require('./idade');
+let nomefuncao = require('./nome');
+
+console.log(estadofuncao("SP"));
+console.log(idadefuncao(19));
+console.log(nomefuncao('João'));
